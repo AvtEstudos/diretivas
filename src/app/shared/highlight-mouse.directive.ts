150927efa9d027f85565592ac4b1382ec3a73ac7
AVT Estudos <andretamayoshi@gmail.com>
2019-07-15 19:30:39 -0300

    Aula 34 - P2

--- src/app/shared/highlight-mouse.directive.ts
+++ src/app/shared/highlight-mouse.directive.ts
@@ -6,28 +6,24 @@ import { Directive, HostListener, ElementRef, Renderer, HostBinding } from '@ang
 export class HighlightMouseDirective {
 
   @HostListener('mouseenter') onMouseOver(){
-    this.renderer.setElementStyle( 
-      this.elementRef.nativeElement,
-      'background-color',
-      'yellow'
-    )
+    this.backgroundColor = 'yellow';
   }
 
   @HostListener('mouseleave') onMouseLeave(){
-    this.renderer.setElementStyle( 
-      this.elementRef.nativeElement,
-      'background-color',
-      'white'
-    )
+    this.backgroundColor = 'white';
   }
 
-  @HostBinding('style.backgroundColor') backgroundColor: string;
+  //@HostBinding('style.backgroundColor') backgroundColor: string;
 
-  constructor(
-    private elementRef: ElementRef,
-    private renderer: Renderer
-  ) {
+  @HostBinding('style.backgroundColor') get setColor(){
+    //Código extra caso precise
+    return this.backgroundColor;
+  };
 
-   }
+  private backgroundColor: string;
+
+  constructor() {
+
+  }
 
 }
